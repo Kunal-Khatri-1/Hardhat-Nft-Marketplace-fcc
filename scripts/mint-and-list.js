2b2ca06b805ad4ff3a32d953316e40d519e7fceb
Kunal Khatri <kunalkhatri137@gmail.com>
2022-11-04 00:27:36 +0530

    mint and list NFT to Marketplace added

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,14 +1,31 @@
 // mint and immidiately list on marketplace
+// run a node to run scripts
 
 const { ethers } = require("hardhat")
 
 async function mintAndList() {
-    const nftmarketplace = await ethers.getContract("nftMarketPlace")
+    const nftmarketplace = await ethers.getContract("NftMarketplace")
     const basicNft = await ethers.getContract("BasicNft")
 
     console.log("Minting...")
     const mintTx = await basicNft.mintNft()
     const mintTxReceipt = await mintTx.wait(1)
+    // capturing the tokenId in Event
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+
+    console.log("Approving marketplace for the NFT...")
+    const approvalTx = await basicNft.approve(nftmarketplace.address, tokenId)
+    await approvalTx.wait(1)
+
+    console.log("Listing NFT...")
+    const listingTx = await nftmarketplace.listItem(
+        basicNft.address,
+        tokenId,
+        ethers.utils.parseEther("1")
+    )
+    await listingTx.wait(1)
+
+    console.log("NFT listed!!!")
 }
 
 mintAndList()
